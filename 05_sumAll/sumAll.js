ed06266ae90b78cec082c55a5acdfa4c894afaec
ichanmsngkyy <ichanmsngky@gmail.com>
2024-10-21 01:57:43 +0800

    Implement sumAll function
    -Create a function that calculates the sum of numbers between firstNumber and secondNumber
    -Update unit test in sumAll.spec.js to check the function behavior

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
+    if(firstNumber < 0 || secondNumber < 0){
+        return 'ERROR';
+    }
+    
+    if(!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)){
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    if(firstNumber > secondNumber){
+        [firstNumber, secondNumber] = [secondNumber, firstNumber];
+    }
+
+    for(let i = firstNumber; i <= secondNumber; i++){
+        
+        sum += i;
+
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
