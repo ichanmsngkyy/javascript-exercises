933619856a940783d5c28ba098b4785e78b4fe18
ichanmsngkyy <ichanmsngky@gmail.com>
2024-10-20 21:10:53 +0800

    Implement reverseString function
    -Implement reverseString by using loops and array manipulation
    -Update unit test in reverseString.spec.js to validate function behavior

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverseString = "";
 
+    for(let i = word.length - 1; i >= 0; i--){
+        reverseString += word[i];
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
