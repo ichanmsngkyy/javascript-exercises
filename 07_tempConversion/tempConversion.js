d8ae3eed1f6cb61a04cf8c35389b8368b764b3cd
ichanmsngkyy <ichanmsngky@gmail.com>
2024-10-21 02:55:37 +0800

    Implement function that can convert temperature
    -Create a function that converts Fahrenheit to Celsius  and vice versa
    -Update unit test tempConversion.spec.js to verify functionalities.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let celsius = (temp - 32) * (5/9);
+  const roundedTemp = Math.round(celsius * 10) / 10;
+  
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  
+  let fahrenheit = (temp * (9/5)) + 32;
+  const roundedTemp = Math.round(fahrenheit * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
