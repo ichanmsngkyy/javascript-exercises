731564efaec2bb3b1d7e7ee5178448a777a09ca9
ichanmsngkyy <ichanmsngky@gmail.com>
2024-10-28 00:07:06 +0800

    Finish all the required exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count;
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
+
+    if(count < 0) return 'OOPS';
+    if(count == 0) return 0;
+
+    let firstNumber = 1;
+    let secondNumber = 0;
+
+    for(let i = 1; i <= count; i++){
+        let current = firstNumber + secondNumber;
+        secondNumber = firstNumber;
+        firstNumber = current;
+    }
+
+    return secondNumber;
+
 
 };
 
